18ae18239b48b40f2b2171cada49742737e5457b
usamaadil16 <104248079+usamaadil16@users.noreply.github.com>
2023-03-24 14:29:34 +0500

    Updates

---
+++ precache-manifest.622580d4c0bda187f96de5c5405bd375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf0d99f23940de4b22b",
+    "url": "/netflix-proj/static/css/main.715f7983.chunk.css"
+  },
+  {
+    "revision": "dcf0d99f23940de4b22b",
+    "url": "/netflix-proj/static/js/main.16a273cc.chunk.js"
+  },
+  {
+    "revision": "40a5716e61a98789adc1",
+    "url": "/netflix-proj/static/js/runtime~main.260ca48f.js"
+  },
+  {
+    "revision": "08e1c426f7c495f31b03",
+    "url": "/netflix-proj/static/js/2.f9627ae6.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/netflix-proj/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "d34dbb3df48f09ee46f418b4100bc7e3",
+    "url": "/netflix-proj/index.html"
+  }
+];
